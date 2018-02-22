1fd2966c9c8cbbc2930c1eb0942d7ba12c613147
Greg <greg@Gregs-MacBook-Pro.local>
2018-02-22 07:34:39 -0500

    Updated

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ from bs4 import BeautifulSoup
 #  dont go overboard on my calls! No more than 1 a second! :U
 #  Only scrape from sites that allow their data to be scraped in their terms 
 
+# Documentation:
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all
+
+# Look into a scraping framework like Scrapy!
+# https://doc.scrapy.org/en/1.5/intro/overview.html
+
 #toscrape.com is a really good resource for learning scraping!
 page_url = "http://quotes.toscrape.com/tag/inspirational/"
 
@@ -17,8 +23,10 @@ page = urlopen(page_url)
 
 soup = BeautifulSoup(page, 'html.parser')
 
+quoteArray = []
 quotes = soup.find_all('span', attrs={'class': 'text'})
 
-quote = quotes[0].contents[0]
+for x in quotes:
+    quoteArray.append(x.text)
 
-print(quote)
+print(quoteArray)
